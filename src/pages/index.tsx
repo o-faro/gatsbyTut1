36d813f9d0295752bec2f1319f9b1aa4ef354785
Oliver Faro <oliver.faro@diva-e.com>
2023-03-01 18:22:04 +0100

    dog added

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,11 +1,16 @@
 import * as React from "react"
 import { HeadFC, Link, PageProps } from "gatsby"
 import Layout from "../components/layout"
+import { StaticImage } from 'gatsby-plugin-image'
 
 const IndexPage: React.FC<PageProps> = () => (
   <main>
     <Layout pageTitle='Home Page'>
       <p>Gatsby Tut first page</p>
+      <StaticImage
+        alt="Clifford, a reddish-brown pitbull, posing on a couch and looking stoically at the camera"
+        src="https://pbs.twimg.com/media/E1oMV3QVgAIr1NT?format=jpg&name=large"
+      />
     </Layout>
   </main>
 )
